c9a0362c30e3a4673b1e38a3745c79801840816d
Luis Cruz <lycruzc@yahoo.com>
2021-09-25 10:17:55 -0500

    client console

---
+++ client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+var client = new HttpClient();
+
+
+var weatherForecasts =
+    await client.GetFromJsonAsync<List<WeatherForecast>>(
+        "https://localhost:7138/weatherforecast");
+
+
+//Console.WriteLine(weatherForecasts);
+
+foreach (var weatherForecast in weatherForecasts)
+{
+    Console.WriteLine(
+        $"Date:{weatherForecast.Date}, TemperatureC:{weatherForecast.TemperatureC}, Summary:{weatherForecast.Summary}");
+}
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
